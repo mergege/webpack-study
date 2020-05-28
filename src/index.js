31b0ff70669fe146ac4504122ab3afddd75ff4af
zhengyh001 <zhengyhoo1@cmrh.com>
2020-05-28 17:44:31 +0800

    webpack优化打包项目

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 
 import './css/index.css'
-import css from './css/index.less'
+import './css/index.less'
+import logo from './images/logo.jpg'
+
+const img = new Image()
+img.src = logo
+document.getElementById('root').appendChild(img)
+console.log(1111)
 // import axios from 'axios'
 // console.log(css)
 
@@ -26,15 +32,35 @@ import css from './css/index.less'
 // })
 
 // js的HMR:hot module replacement热模块替换
-import number from './number.js'
-import counter from './counter.js'
+// import number from './number.js'
+// import counter from './counter.js'
 
-counter()
-number()
+// counter()
+// number()
 
-if (module.hot) {
-  module.hot.accept('./number.js', () => {
-    document.body.removeChild(document.getElementById('number'))
-    number()
-  })
-}
+// if (module.hot) {
+//   module.hot.accept('./number.js', () => {
+//     document.body.removeChild(document.getElementById('number'))
+//     number()
+//   })
+// }
+
+// babel语法转换
+// babel插件分两种：一种是语法转换，一种是语法解析
+// import '@babel/polyfill'：不会转换我们的语法，而是把语法放到window.promise全局对象上---会有全局对象（变量）污染
+// const arr = [new Promise(() => { }), new Promise(() => { })]
+
+// arr.map(item => {
+//   console.log(item)
+// })
+
+// preset-react解析react语法，jsx
+// import React, { Component } from 'react'
+// import ReactDom from 'react-dom'
+
+// class App extends Component {
+//   render () {
+//     return <div>hello react</div>
+//   }
+// }
+// ReactDom.render(<App />, document.getElementById('root'))
